0c0ebeeb961aff6e85afad3c6138decfd385b909
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2026-04-07 18:54:52 -0600

    Instalación y configuracion

---
+++ bases/app.js
@@ -0,0 +1,8 @@
+const msg = "Hola Mundoa";
+const hero = {
+    name: "Iroman",
+    age: 25,
+    power: "Fire",
+};
+console.log(hero.name);
+export {};
---
+++ bases/app.ts
@@ -0,0 +1,7 @@
+const msg: string = "Hola Mundoa";
+const hero = {
+  name: "Iroman",
+  age: 25,
+  power: "Fire",
+};
+console.log(hero.name);
